e34c773e75ecab24ea0686468e78c9e96d761613
mail_yanpeng@163.com <yyyq94581017>
2018-08-03 11:25:38 +0800

    test rx

--- app/src/main/java/com/lvguo/common/MainActivity.java
+++ app/src/main/java/com/lvguo/common/MainActivity.java
@@ -1,7 +1,14 @@
 package com.lvguo.common;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.lvguo.nelson.permissions.RxPermissions;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RxPermissions permissions = new RxPermissions(this);
+        permissions.request(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.WRITE_EXTERNAL_STORAGE).subscribe(new Observer<Boolean>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+            }
+
+            @Override
+            public void onNext(Boolean aBoolean) {
+            }
+
+            @Override
+            public void onError(Throwable e) {
+            }
+
+            @Override
+            public void onComplete() {
+                Toast.makeText(MainActivity.this,"success",Toast.LENGTH_LONG);
+            }
+        });
     }
 }
